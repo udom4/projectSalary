8e06afe4bce32b77be08d40078db294862612c95
udom4 <75348432+udom4@users.noreply.github.com>
2020-12-09 16:25:00 +0700

    Create 2020_12_09_092412_create_ot_rate_table.php

---
+++ database/migrations/2020_12_09_092412_create_ot_rate_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOtRateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ot_rate', function (Blueprint $table) {
+            $table->increments('id');
+            $table->decimal('rate', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ot_rate');
+    }
+}
