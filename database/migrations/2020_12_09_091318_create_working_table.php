4702d3d7a48cef376232aaba7d0e5b00625a7328
udom4 <75348432+udom4@users.noreply.github.com>
2020-12-09 16:22:23 +0700

    Create 2020_12_09_091318_create_working_table.php

---
+++ database/migrations/2020_12_09_091318_create_working_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('working', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('emp_id');
+            $table->foreign('emp_id')->references('id')->on('employee');
+            $table->date('date_working');
+            $table->time('start_working');
+            $table->time('finish_working');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('working');
+    }
+}
