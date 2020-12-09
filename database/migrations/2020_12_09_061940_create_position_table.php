31cabd5bbc80869708d43cbcc91e126d9d12abe6
udom4 <75348432+udom4@users.noreply.github.com>
2020-12-09 13:30:23 +0700

    add position & relation to database

---
+++ database/migrations/2020_12_09_061940_create_position_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePositionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('position', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('pos_name');
+            $table->integer('team_id')->unsigned();
+            $table->foreign('team_id')->references('id')->on('team');
+            $table->decimal('salary_rate',10,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('position');
+    }
+}
