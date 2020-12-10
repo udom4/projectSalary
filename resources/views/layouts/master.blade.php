d6475f167c9bde96c9e76ee5fbc032a47c8bcb9d
udom4 <75348432+udom4@users.noreply.github.com>
2020-12-10 17:30:37 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,15 +6,15 @@
   <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
   <meta name="author" content="Creative Tim">
   <!-- Favicon -->
-  <link rel="icon" href="assets/img/brand/vayo.jpg" type="image/png">
+  <link rel="icon" href="/assets/img/brand/vayo.jpg" type="image/png">
   <!-- Fonts -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
   <!-- Icons -->
-  <link rel="stylesheet" href="assets/vendor/nucleo/css/nucleo.css" type="text/css">
-  <link rel="stylesheet" href="assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+  <link rel="stylesheet" href="/assets/vendor/nucleo/css/nucleo.css" type="text/css">
+  <link rel="stylesheet" href="/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
   <!-- Page plugins -->
   <!-- Argon CSS -->
-  <link rel="stylesheet" href="assets/css/argon.css?v=1.2.0" type="text/css">
+  <link rel="stylesheet" href="/assets/css/argon.css?v=1.2.0" type="text/css">
 </head>
 <body>
 @section('sidebar')
@@ -24,7 +24,7 @@
       <!-- Brand -->
       <div class="sidenav-header  align-items-center">
         <a class="navbar-brand" href="/">
-          <img src="assets/img/brand/vayo.jpg" class="navbar-brand-img" alt="...">
+          <img src="/assets/img/brand/vayo.jpg" class="navbar-brand-img" alt="...">
         </a>
       </div>
       <div class="navbar-inner">
