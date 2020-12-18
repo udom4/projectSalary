1be1e882d03fd5eeb13e9a165cc77c0adcde4027
udom4 <75348432+udom4@users.noreply.github.com>
2020-12-18 08:54:58 +0700

    Update create_employee.blade.php

--- resources/views/employee/create_employee.blade.php
+++ resources/views/employee/create_employee.blade.php
@@ -319,6 +319,5 @@
     });
 </script>
 @endsection
-
 <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
 <script src="http://code.jquery.com/jquery-3.4.1.js"></script>
